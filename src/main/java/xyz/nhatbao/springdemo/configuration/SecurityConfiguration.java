3cf184de7b6158a1c9e56516a751fb36562afe66
mrbaoct-dev <mrbaoct.dev@gmail.com>
2021-01-04 00:34:41 +0700

    Database Handling (with H2 DB)

---
+++ src/main/java/xyz/nhatbao/springdemo/configuration/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package xyz.nhatbao.springdemo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    //    Accept h2-console (Spring Security)
+    //    Don't use this configuration in a production environment
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+}
