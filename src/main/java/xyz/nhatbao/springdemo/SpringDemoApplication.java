a68da39a56a5352d15c854e10767805ec9a36569
mrbaoct-dev <mrbaoct.dev@gmail.com>
2021-01-03 22:54:50 +0700

    Application Properties

--- src/main/java/xyz/nhatbao/springdemo/SpringDemoApplication.java
+++ src/main/java/xyz/nhatbao/springdemo/SpringDemoApplication.java
@@ -1,16 +1,25 @@
 package xyz.nhatbao.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class SpringDemoApplication {
     /*The @ComponentScan annotation is used to find beans and the corresponding injected with @Autowired annotation.*/
     @Autowired
     private RestTemplate restTemplate;
+
+    //    @Value annotation is used to read the environment or application property value
+    //    @Value("${spring.application.name:spring-boot-demo}") to set default value if the property is not found
+    @Value("${spring.application.name}")
+    private String name;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoApplication.class, args);
@@ -22,5 +31,11 @@ public class SpringDemoApplication {
     public RestTemplate getRestTemplate() {
         System.out.println("Return New Rest Template");
         return new RestTemplate();
+    }
+
+    //    Show the application name
+    @RequestMapping(value = "/")
+    public String name() {
+        return "Application name: " + name;
     }
 }
