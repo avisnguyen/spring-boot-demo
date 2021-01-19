626a983971a50398d3a6b7de9d80c9ff55382522
mrbaoct-dev <mrbaoct.dev@gmail.com>
2021-01-19 22:32:10 +0700

    Scheduling

--- src/main/java/xyz/nhatbao/springdemo/SpringDemoApplication.java
+++ src/main/java/xyz/nhatbao/springdemo/SpringDemoApplication.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@EnableScheduling
 @SpringBootApplication
 @RestController
 public class SpringDemoApplication {
